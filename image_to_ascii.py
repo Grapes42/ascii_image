b62813bd5bcfb981e772da27e43ab836d2840432
Grapes42 <f.inggrapes@gmail.com>
2024-07-06 18:27:20 +1200

    increasing ease of use and robustness

--- image_to_ascii.py
+++ image_to_ascii.py
@@ -1,27 +1,90 @@
 import pickle
 import sys
+import os.path
 import json
 import math
 from PIL import Image
 
+#
+# Functions
+#
+
+# check if a dimension argument is valid
+def dimension_check(dimension, arg):
+    try:
+        dimension = int(sys.argv[sys.argv.index(arg)+1])
+
+        if dimension < 0:
+            print(f"{dimension} should be greater than 0")
+    except:
+        print(f"Invalid image {dimension}")
+        exit()
+
+    return dimension
+
+# return closest value in list
 def closest(lst, K):
     return lst[min(range(len(lst)), key = lambda i: abs(lst[i]-K))]
 
+# return key for a value in dictionary
 def key_from_val(dictionary, index):
     return list(dictionary.keys())[list(dictionary.values()).index(index)]
 
-with open("gradient.json", "r") as f:
-    gradient = json.load(f)
+#
+# Command line parameters
+#
 
+help_message = """Format command as such:\npython3 -path <image path> -height <height> -width <width>
+Width and height are optional, if left default image resolution will be kept"""
+
+# check if a path has been provided
+if "-path" not in sys.argv:
+    print(f"Image not provided. {help_message}")
+    exit()
+
+# iterate over command line arguments
+for arg in sys.argv:
+    # path variable check
+    if arg == "-path":
+        try:
+            image = sys.argv[sys.argv.index(arg)+1]
+        except:
+            print(f"Image not provided. {help_message}")
+    # help        
+    elif arg == "-help":
+        print(help_message)
+
+    # height variable check
+    elif arg == "-height":
+        height = dimension_check(dimension="Height", arg=arg)
+
+    # width variable check
+    elif arg == "-width":
+        width = dimension_check(dimension="Width", arg=arg)
+
+# check if image is valid
+if not os.path.isfile(image):
+    print("Invalid image path")
+    exit()
+
+# load gradient from json
+gradient_file = "gradient.json"
+
+if os.path.isfile(gradient_file):
+    with open(gradient_file, "r") as f:
+        gradient = json.load(f)
+else:
+    print("No gradient file exists, create gradient with $ python3 sort_to_gradient.py")
+
+
+
+# waiting message
 print("low quality ascii hideo kojima is thinking")
-
-image = sys.argv[1]
 
 im = Image.open(image, "r")
 width, height = im.size
 
 height = math.floor(height/2)
-
 
 rgb_vals = list(im.getdata())
 
@@ -33,8 +96,6 @@ for y in range(height):
 
     for x in range(width): # iterate over entire line
         rgb_vals.pop(skip)
-
-
 
 brightness_vals = []
 
@@ -69,23 +130,25 @@ scale_factor = gradient_max_brightness / shifted_image_max_brightness
 # constructing time!!
 pixel_count = width*(height)
 
+# if no output name is provided, set a default
 if len(sys.argv) < 3:
     image_out = "image.asi"
 else:
     image_out = f"{sys.argv[2]}"
 
-asi = [height, width]
+asi = [height, width] # set first 2 bytes of asi file as the height and width
 
 pos = 0
 
-for y in range(height):
-    for x in range(width):
-        pos_brightness = brightness_vals[pos] * scale_factor
+# Go through all pixels and assign the character with the closest brightness value to it
+for y in range(height): # for each row
+    for x in range(width): # for each column in row
 
-        # compare brightness value of the current pixel and pick the closest 
-        value = closest( list(gradient.values()), pos_brightness)
+        pos_brightness = brightness_vals[pos] * scale_factor # scale image pixel to fit in the character brightness range
+
+        value = closest( list(gradient.values()), pos_brightness) # compare brightness value of the current pixel and pick the closest 
     
-        char_code = key_from_val(gradient, value)
+        char_code = key_from_val(gradient, value) # pick a character code based on the sorted gradient dictionary
 
         asi.append(char_code)
 
@@ -95,5 +158,6 @@ for y in range(height):
 
         pos += 1
 
+# dump asi info to file
 with open(image_out, "wb") as f:
     pickle.dump(asi, f)
